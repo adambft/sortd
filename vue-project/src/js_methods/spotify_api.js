893dacc9316e44d9a17ce1bfb4c3f341401977b6
Adam Bin Firdaus Tan <adambft98@gmail.com>
2023-12-31 23:46:56 +0800

    changes made

--- vue-project/src/js_methods/spotify_api.js
+++ vue-project/src/js_methods/spotify_api.js
@@ -1,5 +1,26 @@
+import axios from 'axios';
+
 export const SpotifyApiUtils = {
-    testFunc() {
-        return "you fucking did it motherfucker"
-    }
+    async getAccessToken() {
+        try {
+            const response = await axios.post('https://accounts.spotify.com/api/token', {
+                // Request body data
+                grant_type: 'client_credentials',
+                client_id: '357de24bd7b34a0484553caed20898cd',
+                client_secret: '',
+            }, {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                }
+            });
+
+            // Handle success response
+            console.log(response.data);
+            return response.data;
+        } catch (error) {
+            // Handle error response
+            console.error(error);
+            throw error; // Re-throw the error to be caught by the caller if needed
+        }
+    },
 }
--- vue-project/src/views/SorterView.vue
+++ vue-project/src/views/SorterView.vue
@@ -1,14 +1,31 @@
 <template>
-    <div class="main">
-      <div>
-        <h1>Welcome to The Actual Fucking App</h1>
-      </div>
+  <div class="main">
+    <div>
+      <h1>Welcome to The Actual Fucking App</h1>
+      <p>{{ testparam }}</p>
     </div>
-  </template>
-  
-  <style scoped>
-    .main {
-      padding: 1.2rem;
-      background-color: #c6ffdc;
-    }
-  </style>
+  </div>
+</template>
+
+<script>
+
+import { SpotifyApiUtils } from '../js_methods/spotify_api'
+
+export default {
+  data() {
+    return {
+      testparam: "",
+    };
+  },
+  async mounted() {
+    this.testparam = await SpotifyApiUtils.getAccessToken()
+  }
+};
+</script>
+
+<style scoped>
+.main {
+  padding: 1.2rem;
+  background-color: #c6ffdc;
+}
+</style>
