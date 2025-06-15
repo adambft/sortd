a89de408821126a22286d859081e45ff461ee9f5
Adam Bin Firdaus Tan <adambft98@gmail.com>
2025-06-15 12:51:43 +0800

    debug prod mode not working

--- src/main.js
+++ src/main.js
@@ -1,31 +1,80 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import bootstrapPopovers from './js_methods/bootstrap_popover'
+import bootstrapPopovers from "./js_methods/bootstrap_popover";
 
 /* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from "@fortawesome/fontawesome-svg-core";
 
 /* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 /* import specific icons */
-import { faArrowRight, faBackward, faCircleCheck, faCirclePlus, faCircleQuestion, faFloppyDisk, faForward, faGear, faHandPointer, faListUl, faMagnifyingGlass, faMusic, faPause, faPlay, faShopLock, faShuffle, faSpinner, faThumbsUp, faUserCircle, faVolumeHigh } from '@fortawesome/free-solid-svg-icons'
+import {
+	faArrowRight,
+	faBackward,
+	faCircleCheck,
+	faCirclePlus,
+	faCircleQuestion,
+	faFloppyDisk,
+	faForward,
+	faGear,
+	faHandPointer,
+	faListUl,
+	faMagnifyingGlass,
+	faMusic,
+	faPause,
+	faPlay,
+	faShopLock,
+	faShuffle,
+	faSpinner,
+	faThumbsUp,
+	faUserCircle,
+	faVolumeHigh,
+} from "@fortawesome/free-solid-svg-icons";
 
 /* add icons to the library (ADD NEW ICONS HERE v) */
-library.add(faSpinner, faArrowRight, faMusic, faListUl, faHandPointer, faSpotify, faUserCircle, faCircleQuestion, faFloppyDisk, faCircleCheck, faCirclePlus, faMagnifyingGlass, faPlay, faPause, faForward, faBackward, faVolumeHigh, faGear, faThumbsUp, faShuffle, faShopLock)
+library.add(
+	faSpinner,
+	faArrowRight,
+	faMusic,
+	faListUl,
+	faHandPointer,
+	faSpotify,
+	faUserCircle,
+	faCircleQuestion,
+	faFloppyDisk,
+	faCircleCheck,
+	faCirclePlus,
+	faMagnifyingGlass,
+	faPlay,
+	faPause,
+	faForward,
+	faBackward,
+	faVolumeHigh,
+	faGear,
+	faThumbsUp,
+	faShuffle,
+	faShopLock
+);
 
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import { faSpotify } from '@fortawesome/free-brands-svg-icons'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { faSpotify } from "@fortawesome/free-brands-svg-icons";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
-app.use(bootstrapPopovers)
-app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(router);
+app.use(bootstrapPopovers);
+app.component("font-awesome-icon", FontAwesomeIcon);
 
-app.mount('#app')
+app.mount("#app");
+
+// For debugging when on development mode
+if (import.meta.env.MODE === "development") {
+	// Dev-only logic
+	console.log("\n\nRunning in dev mode ===========================\n\n");
+}
